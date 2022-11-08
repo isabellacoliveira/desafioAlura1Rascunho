1a17dfef6a5e4ba38ab1d9268c9592fdfe265750
Isabella Cruz de Oliveira <isaaholiveira15@gmail.com>
2022-11-08 15:58:05 -0300

    implementando o highlight js

--- highlight.js
+++ highlight.js
@@ -1,13 +1,25 @@
-const linguagem = document.querySelector('.linguagem')
-const areaDoCodigo = document.querySelector('#caixa')
-const codiguinho = document.getElementsByTagName('code')
-const selecionaBotao = document.querySelector('#botao')
 
+const linguagem = document.querySelector('.linguagem')
+console.log(linguagem)
+
+// chamar a div 
+const areaDoCodigo = document.querySelector('.preservar')
+console.log(areaDoCodigo) 
+
+// const codiguinho = document.getElementsByTagName('code')
+// console.log(codiguinho) 
+
+const selecionaBotao = document.querySelector('#botao')
+console.log(selecionaBotao) 
+
+// não funciona
+// const barra = document.getElementsByClassName('.barraSuperior')
+// console.log(barra)
 
 function aplicaHighlight() {
     const codigo = areaDoCodigo.innerText;
-    areaDoCodigo.innerHTML = `<code class="preview-hljs ${linguagem.value}" contenteditable="true" aria-label="Editor de código"></code>`
-    areaDoCodigo.querySelector("code").textContent = codigo 
+    areaDoCodigo.innerHTML = `<code class="preview-hljs ${linguagem.value}" contenteditable="true" aria-label="Editor de código" ></code>`
+    areaDoCodigo.querySelector("code").textContent = codigo;
     hljs.highlightElement(areaDoCodigo.querySelector("code"))
 }
 
