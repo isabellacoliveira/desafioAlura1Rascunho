23b51bb4b9598541e7b3c2d5c9dcf4002c8684b8
Isabella Cruz de Oliveira <isaaholiveira15@gmail.com>
2022-11-09 09:29:02 -0300

    organizando os códigos

---
+++ javascript/sumir.js
@@ -0,0 +1,13 @@
+// usaremos o javascript para mudar o conteúdo que aparece ao passar o mouse por cima 
+function mOver(obj) {
+    obj.innerHTML="Obrigado"
+}
+
+function mOut(obj) {
+    obj.innerHTML="Passe o mouse em mim"
+}
+
+
+
+
+
